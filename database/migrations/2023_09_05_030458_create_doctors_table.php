26dbab5dc9ab86ac2150931ee4bdbafb9f195e46
fransfilastap@live.com <fransfilastap@Live.com>
2023-10-30 11:31:19 +0700

    fix migration

--- database/migrations/2023_09_05_030458_create_doctors_table.php
+++ database/migrations/2023_09_05_030458_create_doctors_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->softDeletes();
             $table->timestamps();
 
-            $table->foreign('user_id')->on('id')->references('users');
+            $table->foreign('user_id')->on('users')->references('id');
         });
     }
 
